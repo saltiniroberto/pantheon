0d9e106f080ddf0bf081609ac238e1004c8d7c7e
Roberto Saltini <saltini.roberto@gmail.com>
2018-10-18 10:13:49 +1100

    Saved work

--- consensus/ibft/src/main/java/tech/pegasys/pantheon/consensus/ibft/IbftStateMachine.java
+++ consensus/ibft/src/main/java/tech/pegasys/pantheon/consensus/ibft/IbftStateMachine.java
@@ -12,6 +12,12 @@
  */
 package tech.pegasys.pantheon.consensus.ibft;
 
+import tech.pegasys.pantheon.consensus.ibft.ibftevent.IbftMessageEvent;
+import tech.pegasys.pantheon.consensus.ibft.ibftmessagedata.AbstractIbftUnsignedMessageData;
+import tech.pegasys.pantheon.consensus.ibft.ibftmessagedata.IbftSignedMessageData;
+import tech.pegasys.pantheon.consensus.ibft.ibftmessagedata.IbftUnsignedPrePrepareMessageData;
+import tech.pegasys.pantheon.consensus.ibft.ibftmessagedata.IbftUnsignedPrepareMessageData;
+
 /** Stateful evaluator for ibft events */
 public class IbftStateMachine {
 
@@ -26,6 +32,31 @@ public class IbftStateMachine {
    */
   public boolean processEvent(final IbftEvent event, final RoundTimer roundTimer) {
     // TODO: don't just discard the event, do some logic
+    if(event instanceof IbftMessageEvent)
+    {
+      IbftSignedMessageData<?> signedMessageData = ((IbftMessageEvent)event).ibftMessage();
+      AbstractIbftUnsignedMessageData usignedMessageData = signedMessageData.getUnsignedMessageData();
+
+      if(usignedMessageData instanceof IbftUnsignedPrepareMessageData)
+      {
+        return handlePrePrepare((IbftSignedMessageData<IbftUnsignedPrePrepareMessageData>)signedMessageData);
+      }
+      else if(usignedMessageData instanceof IbftUnsignedPrePrepareMessageData)
+      {
+        return handlePrepare((IbftSignedMessageData<IbftUnsignedPrepareMessageData>)signedMessageData);
+      }
+
+    }
+    return true;
+  }
+
+  private static boolean handlePrePrepare(IbftSignedMessageData<IbftUnsignedPrePrepareMessageData> signedMessageData)
+  {
+    return true;
+  }
+
+  private static boolean handlePrepare(IbftSignedMessageData<IbftUnsignedPrepareMessageData> signedMessageData)
+  {
     return true;
   }
 }
